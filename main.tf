694da00b0a862033c69d44d8c63b913910b4499a
David Noland <david.noland@suse.com>
2022-12-05 21:03:53 -0800

    Added outputs for host ips

--- main.tf
+++ main.tf
@@ -132,8 +132,15 @@ resource "rancher2_cluster" "rancher_cluster" {
   count = var.rancher_api_url != "" ? 1 : 0
   description = "${var.hostname_prefix} created by Terraform"
 }
-  
+
 output "harvester_url" {
   value = "https://${equinix_metal_reserved_ip_block.harvester_vip.network}/"
 }
 
+output "seed_ip" {
+  value = data.equinix_metal_device.seed_device.access_public_ipv4
+}
+
+output "join_ips" {
+  value = length(data.equinix_metal_device.join_devices) == 0 ? ["none"] : data.equinix_metal_device.join_devices.*.access_public_ipv4
+}
