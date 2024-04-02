26c2daaeb20d80292ea20c92a6ecf33d5a7d5e3b
David Noland <david.noland@suse.com>
2024-04-01 18:44:38 -0700

    Bumping equinix provider to v1.34.0 and rancher to 4.1.0. New provider requires a description and metro in lower case

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 locals {
   project_id = var.metal_create_project ? equinix_metal_project.new_project[0].id : data.equinix_metal_project.project.project_id
+  metro = lower(var.metro)
 }
 
 // IP attachment to be added to seed node, and this is subsequently assigned as Harvester vip
@@ -21,7 +22,7 @@ resource "equinix_metal_project" "new_project" {
 
 resource "equinix_metal_reserved_ip_block" "harvester_vip" {
   project_id = local.project_id
-  metro      = var.metro
+  metro      = local.metro
   type       = "public_ipv4"
   quantity   = 1
 }
@@ -30,7 +31,7 @@ resource "equinix_metal_device" "seed" {
   hostname         = "${var.hostname_prefix}-1"
   count            = var.node_count >= 1 && !var.spot_instance ? 1 : 0
   plan             = var.plan
-  metro            = var.metro
+  metro            = local.metro
   operating_system = "custom_ipxe"
   billing_cycle    = var.billing_cylce
   project_id       = local.project_id
@@ -43,7 +44,7 @@ resource "equinix_metal_spot_market_request" "seed_spot_request" {
   count            = var.node_count >= 1 && var.spot_instance ? 1 : 0
   project_id       = local.project_id
   max_bid_price    = var.max_bid_price
-  metro            = var.metro
+  metro            = local.metro
   devices_min      = 1
   devices_max      = 1
   wait_for_devices = true
@@ -68,7 +69,7 @@ resource "equinix_metal_device" "join" {
   hostname         = "${var.hostname_prefix}-${count.index + 2}"
   count            = var.spot_instance ? 0 : var.node_count - 1
   plan             = var.plan
-  metro            = var.metro
+  metro            = local.metro
   operating_system = "custom_ipxe"
   billing_cycle    = var.billing_cylce
   project_id       = local.project_id
@@ -81,7 +82,7 @@ resource "equinix_metal_spot_market_request" "join_spot_request" {
   count            = var.spot_instance ? var.node_count - 1 : 0
   project_id       = local.project_id
   max_bid_price    = var.max_bid_price
-  metro            = var.metro
+  metro            = local.metro
   devices_min      = 1
   devices_max      = 1
   wait_for_devices = true
@@ -97,9 +98,10 @@ resource "equinix_metal_spot_market_request" "join_spot_request" {
 }
 
 resource "equinix_metal_vlan" "vlans" {
-  count      = var.num_of_vlans
-  project_id = local.project_id
-  metro      = var.metro
+  count       = var.num_of_vlans
+  description = "VLAN for ${var.hostname_prefix}"
+  project_id  = local.project_id
+  metro       = local.metro
 }
 
 resource "equinix_metal_port_vlan_attachment" "vlan_attach_seed" {
